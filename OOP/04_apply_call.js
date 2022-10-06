8dee911c19d56de3bf03274e7d6cee1417573d42
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-06 10:53:35 +0330

    add apply call

---
+++ OOP/04_apply_call.js
@@ -0,0 +1,24 @@
+// apply and call method
+
+function Circle(radius) {
+    this.radius = radius;
+    this.move = function () {
+        console.log("move");
+    }
+    return this;
+}
+
+const cirlce0 = new Circle();
+console.log(cirlce0);
+
+const circle1 = Circle.call({}, 5)
+console.log(circle1);
+
+const obj = { name: "Hesam" };
+const circle2 = Circle.call(obj, 6);
+console.log(circle2);
+
+
+const obj2 = { name: "Hesam" };
+const circle3 = Circle.apply(obj2, [7]);
+console.log(circle3);
