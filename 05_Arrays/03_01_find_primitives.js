13caa2ded481111bb846262e99fad79f30d92c7b
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-08 17:19:12 +0330

    find primitive types in array

---
+++ 05_Arrays/03_01_find_primitives.js
@@ -0,0 +1,19 @@
+// Find a primitive Typed item
+const numbers = [1, 2, 3, 4, 1];
+
+
+console.log(numbers)
+console.log(numbers.indexOf(1));
+console.log(numbers.indexOf(5));
+
+
+console.log(numbers.includes(1));
+console.log(numbers.includes(5));
+
+
+
+console.log(numbers.indexOf(1, 1));
+console.log(numbers.includes(2, 3));
+
+
+
