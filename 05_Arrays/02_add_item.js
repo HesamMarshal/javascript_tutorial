b829d07a317dfc4a6e4c9fc9bea2645bd10901c3
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-08 17:18:28 +0330

    add add items to an array

---
+++ 05_Arrays/02_add_item.js
@@ -0,0 +1,25 @@
+
+// add an item to the end
+console.log(numbers)
+numbers.push(1)
+console.log(numbers)
+numbers.push(2, 3)
+console.log(numbers)
+
+// add an item to the start
+
+numbers.unshift(0)
+console.log(numbers)
+numbers.unshift(-1, -2);
+console.log(numbers);
+
+// add an item to the middle
+numbers.splice(1, 0, "x");
+console.log(numbers);
+
+// TODO: Array of strings
+// TODO: Array of arrays
+// TODO: array of objects
+
+
+
