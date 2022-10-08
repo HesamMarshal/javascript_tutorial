420c8f09f1137371e750d6d443e8504120268b51
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-08 17:17:55 +0330

    add basics of arrays

---
+++ 05_Arrays/01_basics.js
@@ -0,0 +1,16 @@
+// Define an array
+const numbers = [];
+const numbers2 = [1, 2];
+console.log(typeof numbers)
+
+// Access items
+// TODO: Add accessing
+
+// array of numbers
+// array of strings
+// array of booleans
+// array of arrays
+// array of objects
+
+// Speak about copy arrays
+
