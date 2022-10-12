b13f84d8f545f149b9146d999021a3de7dfe557c
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-12 09:37:09 +0330

    add join vs split file

---
+++ 05_Arrays/08_join_split.js
@@ -0,0 +1,16 @@
+// 
+
+const myMessage = "This is a JS Course!";
+console.log(myMessage);
+
+const split1 = myMessage.split(" ");
+console.log(split1);
+
+// join
+const joined = split1.join("-");
+console.log(joined);
+
+
+// Chian
+const chainedJoined = myMessage.split(" ").join("-");
+console.log(chainedJoined);
