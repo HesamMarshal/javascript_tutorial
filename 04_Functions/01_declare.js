e4f45421e213a969973ee1370dc18db6d986454d
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-13 07:23:49 +0330

    add declaration of function

---
+++ 04_Functions/01_declare.js
@@ -0,0 +1,24 @@
+// Define a function
+
+// Function Declaration
+function speak() {
+    console.log("Speak!");
+}
+
+// call the function
+speak();
+
+// Function Expresion
+const run = function () {
+    console.log("Runnnn !");
+}
+
+run();
+
+// Hoisting
+// Function Declaration => run before declare
+// Function Expresion => can't run before delare
+
+// TODO: add parameters
+// TODO: return a value, Perform a task,
+// TODO: argumnts vs parameter
