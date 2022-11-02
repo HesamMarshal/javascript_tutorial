91ebbda8555a914b80f475af206e79434b9d08c2
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-11-02 11:24:07 +0330

    add location.js file

---
+++ SPA/js/location.js
@@ -0,0 +1,10 @@
+console.log(location);
+console.log("href:", location.href);
+console.log("origin:", location.origin);
+console.log("protocol:", location.protocol);
+console.log("host:", location.host);
+console.log("hostname:", location.hostname);
+console.log("port:", location.port);
+console.log("pathname", location.pathname);
+
+
