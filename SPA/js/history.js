b7a543aba1f930c825a5e491d1b6036ee00aab93
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-11-03 11:06:59 +0330

    add history.js

---
+++ SPA/js/history.js
@@ -0,0 +1,34 @@
+console.log(history);
+console.log(history.length);
+console.log(history.scrollRestoration);
+console.log(history.state);
+
+// history.pushState()
+
+// [[Prototype]]
+// :
+// History
+// back
+// :
+// ƒ back()
+// forward
+// :
+// ƒ forward()
+// go
+// :
+// ƒ go()
+// length
+// :
+// (...)
+// pushState
+// :
+// ƒ pushState()
+// replaceState
+// :
+// ƒ replaceState()
+// scrollRestoration
+// :
+// (...)
+// state
+// :
+// (...)
